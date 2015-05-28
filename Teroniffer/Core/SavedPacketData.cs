2a78ba31683e028d9dcc9a932a27e61574faf8f2
Detrav <witalyezep@gmail.com>
2015-05-28 20:36:06 +0300

    Добавил новую структуру для сохранения данны�

---
+++ Teroniffer/Core/SavedPacketData.cs
@@ -0,0 +1,23 @@
+﻿using Detrav.TeraApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.Teroniffer.Core
+{
+    class SavedPacketData
+    {
+        public PacketType type { get; set; }
+        public byte[] data { get; set; }
+
+        public SavedPacketData(PacketType type, byte[] data)
+        {
+            this.type = type;
+            this.data = data;
+        }
+
+        public SavedPacketData() : this(PacketType.Any, null) { }
+    }
+}
